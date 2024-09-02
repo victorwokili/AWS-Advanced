9b5f0f1153de9fd10b5a5e6c8c78a8e8a8027bb1
victorwokili <vwokili@gmail.com>
2024-09-02 15:54:59 -0700

    updated user-data

--- maven-sonarqube-nexus-projects-main/user-data-for-maven-ec2-instance.sh
+++ maven-sonarqube-nexus-projects-main/user-data-for-maven-ec2-instance.sh
@@ -3,9 +3,13 @@ sudo amazon-linux-extras install java-openjdk11 -y
 java --version
 sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
-sudo yum install -y apache-maven## Configure MAVEN_HOME and PATH Environment Variables
+sudo yum install -y apache-maven
+
+## Configure MAVEN_HOME and PATH Environment Variables
 rm .bash_profile
 wget https://raw.githubusercontent.com/victorwokili/AWS-Advanced/main/maven-sonarqube-nexus-projects-main/jenkins-master-client-config/.bash_profile
 source .bash_profile
-mvn -v## Install Git SCM
+mvn -v
+
+## Install Git SCM
 yum install git -y
