4fdb32f183137f4b774fa79071fab8a0cdc93bac
victorwokili <vwokili@gmail.com>
2024-09-02 15:44:23 -0700

    updated wget

--- maven-sonarqube-nexus-projects-main/user-data-for-maven-ec2-instance.sh
+++ maven-sonarqube-nexus-projects-main/user-data-for-maven-ec2-instance.sh
@@ -5,7 +5,7 @@ sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-ma
 sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
 sudo yum install -y apache-maven## Configure MAVEN_HOME and PATH Environment Variables
 rm .bash_profile
-wget https://raw.githubusercontent.com/awanmbandi/realworld-cicd-pipeline-project/jenkins-master-client-config/.bash_profile
+wget https://raw.githubusercontent.com/victorwokili/AWS-Advanced/main/maven-sonarqube-nexus-projects-main/jenkins-master-client-config/.bash_profile
 source .bash_profile
 mvn -v## Install Git SCM
 yum install git -y
