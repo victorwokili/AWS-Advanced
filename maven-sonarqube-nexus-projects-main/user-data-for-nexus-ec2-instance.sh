b973a4d235b66a91c8aad08aabf08dbf90732024
victorwokili <vwokili@gmail.com>
2024-09-03 18:40:31 -0700

    working user data for nexus ec2 instance

--- maven-sonarqube-nexus-projects-main/user-data-for-nexus-ec2-instance.sh
+++ maven-sonarqube-nexus-projects-main/user-data-for-nexus-ec2-instance.sh
@@ -1,35 +1,43 @@
 #!/bin/bash
-yum install java-1.8.0-openjdk.x86_64 wget -y   
-mkdir -p /opt/nexus/   
-mkdir -p /tmp/nexus/                           
-cd /tmp/nexus/
-NEXUSURL="https://download.sonatype.com/nexus/3/latest-unix.tar.gz"
-wget $NEXUSURL -O nexus.tar.gz
-EXTOUT=`tar xzvf nexus.tar.gz`
-NEXUSDIR=`echo $EXTOUT | cut -d '/' -f1`
-rm -rf /tmp/nexus/nexus.tar.gz
-rsync -avzh /tmp/nexus/ /opt/nexus/
+
+# Update the system and install OpenJDK 1.8
+yum update -y
+yum install -y java-1.8.0-openjdk
+
+# Create a nexus user with full permission
 useradd nexus
-chown -R nexus.nexus /opt/nexus 
-cat <<EOT>> /etc/systemd/system/nexus.service
-[Unit]                                                                          
-Description=nexus service                                                       
-After=network.target                                                            
-                                                                  
-[Service]                                                                       
-Type=forking                                                                    
-LimitNOFILE=65536                                                               
-ExecStart=/opt/nexus/$NEXUSDIR/bin/nexus start                                  
-ExecStop=/opt/nexus/$NEXUSDIR/bin/nexus stop                                    
-User=nexus                                                                      
-Restart=on-abort                                                                
-                                                                  
-[Install]                                                                       
-WantedBy=multi-user.target                                                      
+echo "nexus ALL=(ALL) NOPASSWD: ALL" | tee -a /etc/sudoers
 
-EOT
+# Download the latest Nexus package
+cd /opt
+wget https://download.sonatype.com/nexus/3/latest-unix.tar.gz
+tar -xvf latest-unix.tar.gz
 
-echo 'run_as_user="nexus"' > /opt/nexus/$NEXUSDIR/bin/nexus.rc
-systemctl daemon-reload
-systemctl start nexus
-systemctl enable nexus
+# Find the extracted directory name
+NEXUS_DIR=$(tar -tf latest-unix.tar.gz | head -1 | cut -d"/" -f1)
+
+# Rename the Nexus directory to a consistent name
+mv $NEXUS_DIR nexus
+
+# Give the nexus user ownership of the Nexus directory
+chown -R nexus:nexus /opt/nexus
+
+# Modify the Nexus configuration to run as the nexus user
+echo 'run_as_user="nexus"' | tee /opt/nexus/bin/nexus.rc
+
+# Create a symbolic link for Nexus
+ln -s /opt/nexus/bin/nexus /etc/init.d/nexus
+
+# Switch to the nexus user and start Nexus
+su - nexus -c "/etc/init.d/nexus start"
+
+# Enable Nexus to start on boot
+chkconfig --add nexus
+chkconfig nexus on
+
+# Retrieve the public IP of the EC2 instance
+PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+
+# Provide the Nexus URL for access
+echo "Nexus is running. Access it at: http://$PUBLIC_IP:8081"
+
